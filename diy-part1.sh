3250ac761e3c48239d33decb545d118dc1bcc7c4
jasonandy999 <59506376+jasonandy999@users.noreply.github.com>
2021-07-02 05:18:30 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,16 +11,14 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+# Add feed sources
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git diy https://github.com/CCnut/feed-netkeeper.git;LUCI-LUA-UCITRACK' feeds.conf.default
+sed -i '$a src-git immortalwrtluci https://github.com/immortalwrt/luci.git;openwrt-18.06-k5.4' feeds.conf.default
+sed -i '$a src-git immortalwrtpackages https://github.com/immortalwrt/packages.git;openwrt-18.06' feeds.conf.default
 
-# Define My Package
-#git clone https://github.com/vernesong/OpenClash package/molun/luci-app-openclash
-#git clone https://github.com/rufengsuixing/luci-app-adguardhome package/molun/luci-app-adguardhome
-#git clone https://github.com/jerrykuku/luci-app-jd-dailybonus package/molun/luci-app-jd-dailybonus
-#git clone https://github.com/sirpdboy/luci-theme-opentopd package/molun/luci-theme-opentopd
-#git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom package/molun/luci-theme-infinityfreedom
+# Add luci-theme-argon
+git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon
+rm -rf ../lean/luci-theme-argon
